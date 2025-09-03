9c9ac0007d4e7d35a83aef6f97a29f5b335cf37f
Mark Lehoczky <marklehoczky@gmail.com>
2025-09-04 00:29:56 +0200

    Implement video display UI

---
+++ VidHub.ViewModels/VideoCollectionViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.ObjectModel;
+using VidHub.Core;
+
+namespace VidHub.ViewModels
+{
+    public class VideoCollectionViewModel
+    {
+        public ObservableCollection<Video> Videos { get; set; }
+    }
+}
