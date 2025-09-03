dbb0eae828f207ea1406e72ba91a533ba574d12f
Mark Lehoczky <marklehoczky@gmail.com>
2025-09-04 00:00:22 +0200

    Add project files.

---
+++ VidHub.WinUI/MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+using Microsoft.UI.Xaml;
+
+namespace VidHub.WinUI
+{
+    public sealed partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
