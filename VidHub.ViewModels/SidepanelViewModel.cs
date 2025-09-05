55bc8e85fd305e600203f07cf6b61f5ecda43c06
Mark Lehoczky <marklehoczky@gmail.com>
2025-09-05 22:27:31 +0200

    Implement basic transfer displaying

--- VidHub.ViewModels/SidepanelViewModel.cs
+++ VidHub.ViewModels/SidepanelViewModel.cs
@@ -1,59 +1,93 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using VidHub.Platform;
+using VidHub.Services.Base.Interfaces;
 using VidHub.Services.Logics.Interfaces;
 
 namespace VidHub.ViewModels
 {
-    public partial class SidepanelViewModel(IVideoOrganizeService service) : ObservableRecipient
+    public partial class SidepanelViewModel(IVideoOrganizeService organizeService, IVideoLoadService loadService) : ObservableRecipient
     {
-        public IEnumerable<string> SortOptions => service.GetSortOptions();
+        #region Organizer section
+        public IEnumerable<string> SortOptions => organizeService.GetSortOptions();
 
         public string? CurrentSortOption
         {
-            get => service.CurrentSortOption;
-            set => service.CurrentSortOption = value;
+            get => organizeService.CurrentSortOption;
+            set => organizeService.CurrentSortOption = value;
         }
 
 
         public string? SearchText
         {
-            get => service.SearchText;
-            set => service.SearchText = value;
+            get => organizeService.SearchText;
+            set => organizeService.SearchText = value;
         }
 
         public bool FilterDate
         {
-            get => service.FilterDate;
-            set => service.FilterDate = value;
+            get => organizeService.FilterDate;
+            set => organizeService.FilterDate = value;
         }
         public DateTimeOffset? StartDate
         {
-            get => service.StartDate;
-            set => service.StartDate = value;
+            get => organizeService.StartDate;
+            set => organizeService.StartDate = value;
         }
         public DateTimeOffset? EndDate
         {
-            get => service.EndDate;
-            set => service.EndDate = value;
+            get => organizeService.EndDate;
+            set => organizeService.EndDate = value;
         }
 
         public bool FilterDuration
         {
-            get => service.FilterDuration;
-            set => service.FilterDuration = value;
+            get => organizeService.FilterDuration;
+            set => organizeService.FilterDuration = value;
         }
         public TimeSpan? MinDuration
         {
-            get => service.MinDuration;
-            set => service.MinDuration = value;
+            get => organizeService.MinDuration;
+            set => organizeService.MinDuration = value;
         }
         public TimeSpan? MaxDuration
         {
-            get => service.MaxDuration;
-            set => service.MaxDuration = value;
+            get => organizeService.MaxDuration;
+            set => organizeService.MaxDuration = value;
+        }
+        #endregion
+
+
+        #region Transfer section
+        public string TransferDescription => loadService.TransferDescription;
+        public bool HasTransfer => loadService.HasTransfer;
+        public bool HasActiveTransfer => loadService.HasActiveTransfer;
+        public int LoadedCount => loadService.LoadedCount;
+        public int TotalCount => loadService.TotalCount;
+        public bool Indeterminate => TotalCount - LoadedCount == 0;
+        #endregion
+
+
+        public SidepanelViewModel() : this(
+            Context.MainHost.GetService<IVideoOrganizeService>(),
+            Context.MainHost.GetService<IVideoLoadService>())
+        {
+            Context.MainHost.GetService<IMainService>().SubscribeToUpdateEvent(UpdateProperties);
+        }
+
+        ~SidepanelViewModel()
+        {
+            Context.MainHost.GetService<IMainService>().UnsubscribeFromUpdateEvent(UpdateProperties);
         }
 
 
-        public SidepanelViewModel() : this(Context.MainHost.GetService<IVideoOrganizeService>()) { }
+        private void UpdateProperties()
+        {
+            OnPropertyChanged(nameof(TransferDescription));
+            OnPropertyChanged(nameof(HasTransfer));
+            OnPropertyChanged(nameof(HasActiveTransfer));
+            OnPropertyChanged(nameof(LoadedCount));
+            OnPropertyChanged(nameof(TotalCount));
+            OnPropertyChanged(nameof(Indeterminate));
+        }
     }
 }
