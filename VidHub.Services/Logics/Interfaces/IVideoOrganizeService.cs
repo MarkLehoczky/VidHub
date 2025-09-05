ce8a52024839d0fd9ebfcfa9f7688fca55afe8dc
Mark Lehoczky <marklehoczky@gmail.com>
2025-09-05 21:40:49 +0200

    Implement basic sorting and filtering

---
+++ VidHub.Services/Logics/Interfaces/IVideoOrganizeService.cs
@@ -0,0 +1,15 @@
+﻿namespace VidHub.Services.Logics.Interfaces
+{
+    public interface IVideoOrganizeService
+    {
+        string? CurrentSortOption { get; set; }
+        string? SearchText { get; set; }
+        bool FilterDate { get; set; }
+        DateTimeOffset? StartDate { get; set; }
+        DateTimeOffset? EndDate { get; set; }
+        bool FilterDuration { get; set; }
+        TimeSpan? MinDuration { get; set; }
+        TimeSpan? MaxDuration { get; set; }
+        IEnumerable<string> GetSortOptions();
+    }
+}
---
+++ VidHub.ViewModels/SidepanelViewModel.cs
@@ -0,0 +1,59 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using VidHub.Platform;
+using VidHub.Services.Logics.Interfaces;
+
+namespace VidHub.ViewModels
+{
+    public partial class SidepanelViewModel(IVideoOrganizeService service) : ObservableRecipient
+    {
+        public IEnumerable<string> SortOptions => service.GetSortOptions();
+
+        public string? CurrentSortOption
+        {
+            get => service.CurrentSortOption;
+            set => service.CurrentSortOption = value;
+        }
+
+
+        public string? SearchText
+        {
+            get => service.SearchText;
+            set => service.SearchText = value;
+        }
+
+        public bool FilterDate
+        {
+            get => service.FilterDate;
+            set => service.FilterDate = value;
+        }
+        public DateTimeOffset? StartDate
+        {
+            get => service.StartDate;
+            set => service.StartDate = value;
+        }
+        public DateTimeOffset? EndDate
+        {
+            get => service.EndDate;
+            set => service.EndDate = value;
+        }
+
+        public bool FilterDuration
+        {
+            get => service.FilterDuration;
+            set => service.FilterDuration = value;
+        }
+        public TimeSpan? MinDuration
+        {
+            get => service.MinDuration;
+            set => service.MinDuration = value;
+        }
+        public TimeSpan? MaxDuration
+        {
+            get => service.MaxDuration;
+            set => service.MaxDuration = value;
+        }
+
+
+        public SidepanelViewModel() : this(Context.MainHost.GetService<IVideoOrganizeService>()) { }
+    }
+}
