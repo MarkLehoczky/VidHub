47948ead180de62dca6a1f81a1d3473c6701cb0d
Mark Lehoczky <marklehoczky@gmail.com>
2025-11-23 19:08:26 +0100

    Fix duration fallback option

--- VidHub.Core/Video.cs
+++ VidHub.Core/Video.cs
@@ -144,7 +144,7 @@ namespace VidHub.Core
                 () => SubtitleStreams = metadataProcessor.GetSubtitleStreams(),
                 () => UnknownStreams = metadataProcessor.GetUnknownStreams(),
                 () => Date = FormatStream.CreationTime != DateTime.MinValue ? FormatStream.CreationTime : File.GetLastWriteTime(FilePath),
-                () => Duration = DefaultVideoStream?.Duration ?? TimeSpan.Zero,
+                () => Duration = DefaultVideoStream?.Duration ?? FormatStream.Duration,
                 () => Title = VidHubSettings.Instance.TitleCustomization.CustomizeTitle(this),
                 ExtractPreviewImage
             ];
